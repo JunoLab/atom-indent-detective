a2f9fecca0f834688c2f915c5806006849cc8c23
aminya <aminyahyaabadi74@gmail.com>
2020-03-09 05:35:04 -0500

    SelectListView defining

--- lib/selector.ts
+++ lib/selector.ts
@@ -1,3 +1,17 @@
 /// <reference path="../types/atom-select-list.d.ts"/>
 import {SelectListView} from 'atom-select-list';
 
+import {TextEditor, CompositeDisposable, Panel, Disposable} from 'atom';
+import {IndentSetting, setIndent, getItemsList} from './indent-detective';
+export function selector_show(subs: CompositeDisposable) {
+
+    
+    let makeModalPanel :boolean = true
+    let modalPanel: Panel
+    let indentListView :SelectListView
+
+    if (makeModalPanel) {
+
+        // Defining a SelectListView with methods - https://github.com/atom/atom-select-list
+        indentListView = new SelectListView({
+
