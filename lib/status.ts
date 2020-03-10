aad6e1c2b6e167635e6834e69d6709de75dc13ab
aminya <aminyahyaabadi74@gmail.com>
2020-03-09 22:53:30 -0500

    Define IndentStatusItem class

--- lib/status.ts
+++ lib/status.ts
@@ -1,8 +1,14 @@
 import {TextEditor} from "atom";
-import {StatusBar} from "atom/status-bar";
+import {StatusBar, Tile} from "atom/status-bar";
 
-export default {
-  activate() {
+export class IndentStatusItem {
+  bar: StatusBar
+  tile: Tile | null
+  text: HTMLElement | null
+  view: HTMLElement
+
+  // Construct a statusbar item - Called only once for one session
+  constructor() {
     return this.createView();
   },
 
@@ -10,27 +16,38 @@ export default {
     return (this.tile != null ? this.tile.destroy() : undefined);
   },
 
-  consumeStatusBar(bar :StatusBar) {
-    this.bar = bar;
+  // Called only once for one session
+  consumeStatusBar(bar: StatusBar) {
+    this.bar = bar
     return this.tile = this.bar.addRightTile({
       item: this.view,
       priority: 10.5
     });
-  },
+  }
 
+  // Called only once for one session
   createView() {
     this.view = document.createElement('span');
     this.view.classList.add('indent-status', 'inline-block');
     this.text = document.createElement('a');
     this.text.innerText = "Spaces (2)";
     this.view.appendChild(this.text);
-    return this.view.onclick = () => {
-      return atom.commands.dispatch(atom.views.getView(atom.workspace.getActiveTextEditor()),
-       'indent-detective:choose-indent');
-    };
-  },
 
-  update(editor? :TextEditor) {
+    this.view.onclick = function () {
+      let editor = atom.workspace.getActiveTextEditor()
+      if (editor){
+        atom.commands.dispatch(atom.views.getView(editor) , 'indent-detective:choose-indent')
+      } // else do nothing
+    }
+
+    // Initial Visibility
+    this.updateDisplay(atom.workspace.getActiveTextEditor())
+
+    return this
+  }
+
+  // Toggles the visibility of statusbar item
+  updateDisplay(editor? :TextEditor | undefined) {
     if (editor) {
       this.view.style.display = "";
       return this.updateText(editor);
@@ -38,8 +55,9 @@ export default {
       this.view.style.display = "none";
       return this.clearText();
     }
-  },
+  }
 
+  // Called from updateDisplay
   updateText(editor :TextEditor) {
     let text;
     if (editor.getSoftTabs()) {
@@ -48,9 +66,15 @@ export default {
       text = "Tabs";
     }
     return (this.text != null ? this.text.innerText = text : undefined);
-  },
+  }
 
+  // Called from updateDisplay
   clearText() {
     return (this.text != null ? this.text.innerText = "" : undefined);
   }
-};
+
+  // Destroy when package is deactivated
+  destroy() {
+    return (this.tile != null ? this.tile.destroy() : undefined);
+  }
+}
