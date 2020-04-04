c8e3d2274f6456e063ed2b476a8cdeb8255d7fad
aminya <aminyahyaabadi74@gmail.com>
2020-04-04 05:31:19 -0500

    beforeEach

--- spec/indent-detective-spec.tsx
+++ spec/indent-detective-spec.tsx
@@ -17,3 +17,33 @@ async function getIndentView(editor: TextEditor): Promise<IndentView> {
     await atom.commands.dispatch(editor.getElement(), "indent-detective:choose-indent")
     return atom.workspace.getModalPanels()[0].getItem()
 }
+
+describe("IndentDetective", () => {
+    let editor: TextEditor
+    let statusBar: StatusBar
+    let indentTile: Tile
+    let indentStatus: HTMLElement
+
+    beforeEach(async () => {
+        jasmine.attachToDOM(atom.views.getView(atom.workspace))
+        // Activate Packages
+        await atom.packages.activatePackage("status-bar")
+        await atom.packages.activatePackage("indent-detective")
+
+        // Open AcuteML.jl
+        editor = await atom.workspace.open(path.join(__dirname, "files", "AcuteML.jl")) as TextEditor
+        expect(editor.element).toHaveFocus()
+
+        // StatusBar
+        statusBar = (document.querySelector("status-bar") as unknown) as StatusBar
+
+        indentTile = statusBar.getRightTiles().slice(-1)[0] // last
+        // @ts-ignore
+        indentStatus = indentTile.getItem()
+        // Wait for status bar service hook
+        while (!indentStatus || !indentStatus.textContent) {
+            indentStatus = document.querySelector(".indent-status")
+        }
+        expect(indentStatus).toBeInstanceOf(HTMLElement)
+    })
+})
